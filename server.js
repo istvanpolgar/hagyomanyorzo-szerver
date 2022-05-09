c20b44ab99daca497250a546c2a481edcce172f4
istvanpolgar <istvanpolgar@yahoo.com>
2022-05-10 01:24:10 +0300

    friss

---
+++ server.js
@@ -0,0 +1,245 @@
+//EXPRESS
+const express = require('express');
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//CORS POLICY
+const cors = require('cors');
+app.use(cors());
+
+//FIREBASE DATABASE
+const { initializeApp } = require('@firebase/app');
+const { getDatabase, ref, set, onValue, child, get  } = require('@firebase/database');
+const firebaseConfig = require('./src/firebaseConfig.js');
+const firebaseApp = initializeApp(firebaseConfig);
+const database = getDatabase(firebaseApp);
+
+//PORT
+const port = process.env.PORT || 8080;
+
+let fs = require('fs');
+
+app.post('/', (req, res) => {
+    let { name, category, event} = req.body;
+
+    if(!name.includes(' '))
+        res.send({
+            message: "Adj meg rendes nevet!"
+        });
+<<<<<<< HEAD
+    else {
+        name= name.replace('.','');
+        name= name.replace('#','');
+        name= name.replace('$','');
+        name= name.replace('[','');
+        name= name.replace(']','');
+
+        get(child(ref(getDatabase()), 'groups/' + event + '/' + name)).then((snapshot) => {
+            if(snapshot.exists()) {
+                res.send({
+                    message: "Már jelentkezett erre az előadásra vagy van valaki más, aki ezt a nevet használta. Kérem adja meg a nevét az apa kezdőbetűjével!"
+                });
+            }
+            else
+            {
+                set(ref(database, 'groups/' + event +  '/' + name), {
+                    class: category
+                });
+                res.send({
+                    message: "Kedves " + name + " sikeresen jelentkeztél a(z) " + event + " eseményre!"
+                });
+            }
+        }).catch((error) => {
+            res.send({
+                message: error
+            });
+        });
+    }
+=======
+    else 
+        if(!category || !event)
+            res.send({
+                message: "Ne hagyj üresen mezőt!"
+            });
+        else {
+            name= name.replace('.','');
+            name= name.replace('#','');
+            name= name.replace('$','');
+            name= name.replace('[','');
+            name= name.replace(']','');
+
+            get(child(ref(getDatabase()), 'groups/' + event + '/' + name)).then((snapshot) => {
+                if(snapshot.exists()) {
+                    res.send({
+                        message: "Már jelentkezett erre az előadásra vagy van valaki más, aki ezt a nevet használta. Kérem adja meg a nevét az apa kezdőbetűjével!"
+                    });
+                }
+                else
+                {
+                    set(ref(database, 'groups/' + event +  '/' + name), {
+                        class: category
+                    });
+                    res.send({
+                        message: "Kedves " + name + " sikeresen jelentkeztél a(z) " + event + " eseményre!"
+                    });
+                }
+            }).catch((error) => {
+                res.send({
+                    message: error
+                });
+            });
+        }
+});
+
+app.post('/saveheadcount', (req, res) => {
+    let { headcount } = req.body;
+
+    set(ref(database, '/headcount'), {
+        nr: headcount
+    });
+    res.send({
+        message: "Létszám módosítva!"
+    });
+>>>>>>> 59b79d2 (Legfrisebb)
+});
+
+app.post('/classes', (req, res) => {  
+    let classes = [];
+    onValue(ref(database, '/classes'), (snapshot) => {
+        snapshot.forEach(cat => {
+            classes.push(cat.val());
+        });
+    }); 
+    res.send({
+        categories: classes
+    });
+});
+
+app.post('/events', (req, res) => {  
+    let events = [];
+<<<<<<< HEAD
+    onValue(ref(database, '/events'), (snapshot) => {
+        snapshot.forEach(ev => {
+            onValue(ref(database, 'groups/' + ev.val()), (snap) => {
+                if(snap.size < 3)
+=======
+    let headcount = 0;
+
+    onValue(ref(database, '/headcount/nr'), (snapshot) => {
+        headcount = snapshot.val();
+    });
+
+    onValue(ref(database, '/events'), (snapshot) => {
+        snapshot.forEach(ev => {
+            onValue(ref(database, 'groups/' + ev.val()), (snap) => {
+                if(snap.size <= headcount)
+>>>>>>> 59b79d2 (Legfrisebb)
+                    events.push(ev.val());
+            });
+        });
+    }); 
+    
+    res.send({
+        events: events
+    });
+});
+
+app.post('/allevents', (req, res) => {  
+    let allevents = [];
+    let events = [];
+    let students;
+    onValue(ref(database, '/events'), (snapshot) => {
+        snapshot.forEach(ev => {
+            events.push(ev.val());
+        });
+    }); 
+
+    events.forEach(ev => {
+        onValue(ref(database, 'groups/' + ev), (snap) => {
+            if(snap.val())
+            {
+                students = [];
+                snap.forEach(st => {
+                    students.push(st.key + ' - ' + st.val().class);
+                });
+                allevents.push({
+                    title: ev,
+                    students: students
+                });
+            }
+        });
+    });
+    res.send({
+        events: allevents
+    });
+});
+
+<<<<<<< HEAD
+=======
+app.post('/headcount', (req, res) => {  
+    let headcount = 0;
+    onValue(ref(database, '/headcount/nr'), (snapshot) => {
+        headcount = snapshot.val();
+    }); 
+    res.send({
+        headcount: headcount
+    });
+});
+
+>>>>>>> 59b79d2 (Legfrisebb)
+app.post('/admin', (req, res) => {  
+    let { user, password } = req.body;
+    if(user == 'adminIstvan' && password == 'Isacson93')
+        res.send({
+            ok: true
+        });
+    else
+        res.send({
+            message: "Helytelen adatokat adtál meg! Próbálkozz újra!"
+        });
+});
+
+app.post('/download', (req, res) => {  
+    let { events } = req.body;
+    let text = "";
+    let i;
+    events.forEach(ev => {
+        text = text + ev.title + '\n';
+        i=1;
+        ev.students.forEach(st => {
+            text = text + i + '. ' + st + '\n';
+            i++;
+        })
+        text = text + '\n\n';
+    });
+    
+    try {
+        if (!fs.existsSync('C://Hagyomanyorzo')) {
+          fs.mkdirSync('C://Hagyomanyorzo');
+        }
+    } catch (err) {
+        res.send({
+            message: err.message
+        });
+    }
+
+    fs.writeFile('C://Hagyomanyorzo/Hagyomanyorzo.txt', text, err => {
+        if (err) {
+            console.log(err);
+            res.send({
+                message: err.message
+            });
+        }
+        else
+        {
+            res.send({
+                message: "A fájl letöltve a C://Hagyomanyorzo mappába!"
+            });
+        }
+      });
+});
+
+app.listen(port, () => {
+    console.log(`Hagyomanyorzo app listening at http://localhost:${port}`);
+})
---
+++ src/firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD1GeZfaicTm5jWxhWJpC7hRV-fEaXCFfI",
+    authDomain: "hagyomanyorzo-3af44.firebaseapp.com",
+    databaseURL: "https://hagyomanyorzo-3af44-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "hagyomanyorzo-3af44",
+    storageBucket: "hagyomanyorzo-3af44.appspot.com",
+    messagingSenderId: "750544245362",
+    appId: "1:750544245362:web:5a7e45b5971acf46d068eb",
+    measurementId: "G-WDSY6M7RJX"
+  };
+  
+  module.exports = firebaseConfig;
